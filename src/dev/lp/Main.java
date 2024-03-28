e9be9fcb01ec24aed3c1ddda7a08bb35b8d9c264
adamd <adam.d.hinton@gmail.com>
2024-03-28 12:28:12 -0400

    Person with kids

--- src/dev/lp/Main.java
+++ src/dev/lp/Main.java
@@ -1,15 +1,25 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+package dev.lp;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+
+        Person jane = new Person();
+        jane.setName("Jane");
+
+        Person jim = new Person();
+        jane.setName("Jim");
+
+        Person joe = new Person();
+        jane.setName("Joe");
+
+        Person john = new Person();
+        jane.setName("John");
+        john.setDob("05/05/1900");
+
+        jane.setKids(new Person[]{jane, jim, joe});
+        System.out.println(john);
+
+
     }
 }
